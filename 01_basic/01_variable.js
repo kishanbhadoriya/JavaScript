076b98d2b5513445c0d6c7a663eec8bc7dfa1573
Kishan Singh <103633372+kishanbhadoriya@users.noreply.github.com>
2024-01-12 13:17:47 +0000

    variable and DataType

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = "123456"
+let emailId = "kishan@gmail.com"
+var accountPassword = "98765"
+accountCity = "Bhind"
+
+console.log(accountId)
+
+let accountState
+emailId = "kishanSingh@gmail.com"
+accountPassword = "2121212121"
+accountCity =  "Benguluru"
+
+console.table([accountId, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,23 @@
+"Use Strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3
+     +3) // Code readability should be High that is not a good practice
+console.log("Kishan") 
+
+let name = "Kishan"
+let age = 30
+let isLogin = true
+
+// number 2 to the power of 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// null => standard value
+// undefined
+// symbol => unique
+
+console.log(typeof age)
+console.log(typeof null) // -- Object
+console.log(typeof undefined) //--- Undefine
