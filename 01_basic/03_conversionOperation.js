833683cf575c90024369f192bb53791c3a4b5ae6
Kishan Singh <103633372+kishanbhadoriya@users.noreply.github.com>
2024-01-14 12:52:26 +0000

    conversion of Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+
+typeof(score)
+// console.log(typeof(score))
+// console.log(typeof score)
+
+let valueAtNumber = Number(score)
+// console.log(valueAtNumber)
+
+let isLogin = 1
+let booleanIsLogin = Boolean(isLogin)
+console.log(booleanIsLogin)
