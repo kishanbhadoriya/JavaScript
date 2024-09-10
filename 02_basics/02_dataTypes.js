c6a2aa98971186966b07db909825789784f69114
Kishan Singh <103633372+kishanbhadoriya@users.noreply.github.com>
2024-09-10 14:07:27 +0000

    Data Type detail description

---
+++ 02_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"Use strict"; //traet all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+/*
+console.log(3 +
+    3
+);
+*/
+// code readablity should be high
+
+let name = "Kishan"
+let age = 18
+let isLogged = false
+let district = null
+let state; // ==> undefined
+
+// number  range => 2 to the power 53
+// bigint
+// String => ""
+// boolean => true/false 
+// null => Standalone value
+// undefined => value is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
